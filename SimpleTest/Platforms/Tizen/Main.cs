bcdadbc056381bfa446fb1c9dac120ae41d76977
Cristian Rangel <cristian.rangel@yardi.com>
2025-05-05 11:18:09 -0700

    Initial code

---
+++ SimpleTest/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace SimpleTest
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
---
+++ SimpleTest/TestPage.cs
@@ -0,0 +1,81 @@
+﻿namespace SimpleTest;
+
+public class testModel
+{
+    public string Name { get; set; }
+}
+
+public class testPicker : Grid
+{
+    private Picker pickerInput;
+
+    // Should work, but doesn't
+    public static readonly BindableProperty ItemDisplayBindingProperty = BindableProperty.Create(nameof(ItemDisplayBinding), typeof(BindingBase), typeof(testPicker), propertyChanged: (bindable, _, newValue) =>
+    {
+        if (bindable is testPicker view)
+        {
+            view.pickerInput.ItemDisplayBinding = (BindingBase)newValue;
+        }
+    });
+
+    public BindingBase ItemDisplayBinding
+    {
+        get => (BindingBase)GetValue(ItemDisplayBindingProperty);
+        set => SetValue(ItemDisplayBindingProperty, value);
+    }
+
+    /*
+    // Also should work, but doesn't
+    public static readonly BindableProperty ItemDisplayBindingProperty = BindableProperty.Create(nameof(ItemDisplayBinding), typeof(Binding), typeof(testPicker), propertyChanged: (bindable, _, newValue) =>
+    {
+        if (bindable is testPicker view)
+        {
+            view.pickerInput.ItemDisplayBinding = (Binding)newValue;
+        }
+    });
+
+    public Binding ItemDisplayBinding
+    {
+        get => (Binding)GetValue(ItemDisplayBindingProperty);
+        set => SetValue(ItemDisplayBindingProperty, value);
+    }
+
+    // Works
+    public static readonly BindableProperty ItemDisplayBindingProperty = BindableProperty.Create(nameof(ItemDisplayBinding), typeof(string), typeof(testPicker), propertyChanged: (bindable, _, newValue) =>
+    {
+        if (bindable is testPicker view)
+        {
+            view.pickerInput.ItemDisplayBinding = new Binding((string)newValue);
+        }
+    });
+    public string ItemDisplayBinding
+    {
+        get => (string)GetValue(ItemDisplayBindingProperty);
+        set => SetValue(ItemDisplayBindingProperty, value);
+    }
+    */
+
+    public testPicker(List<testModel> items)
+    {
+        pickerInput = new() {ItemsSource = items};
+        Add(pickerInput);
+    }
+}
+
+public class TestPage : ContentPage
+{
+    private testPicker picker;
+    public List<testModel> items =
+    [
+        new() {Name = "Test1"},
+        new() {Name = "Test2"},
+        new() {Name = "Test3"},
+    ];
+
+    public TestPage()
+    {
+        picker = new testPicker(items) {ItemDisplayBinding = new Binding("Name")};
+        picker.ItemDisplayBinding = new Binding("Name");
+        Content = new VerticalStackLayout {Children = {picker}};
+    }
+}
